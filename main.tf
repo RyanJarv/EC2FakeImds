5cb9edd12cc33a023ac5386c0bb152aa1087c3a4
RyanJarv <ryan_gerstenkorn@fastmail.fm>
2020-12-03 01:45:26 -0600

    Add file and shell provisioners

--- main.tf
+++ main.tf
@@ -25,35 +25,41 @@ module "ssh_key_pair" {
 
 # WARNING: do not assign a privileged roll to this instance, see notes notes below on the user_data attribute. Really
 # though you shouldn't be running this at all in an account you (or anyone else) actively uses.
-module "instance" {
-  source                      = "git::https://github.com/cloudposse/terraform-aws-ec2-instance.git?ref=master"
-  ssh_key_pair                = module.ssh_key_pair.key_name
-  instance_type               = var.instance_type
-  vpc_id                      = var.vpc_id
-  security_groups             = [aws_security_group.allow_http.id]
-  subnet                      = data.aws_subnet.attacker_subnet.id
-  name                        = "ec2"
-  namespace                   = "jn"
-  stage                       = "sandbox"
-  source_dest_check           = false
-  allowed_ports               = ["80"]
+resource "aws_instance" "imds_server" {
+  subnet_id         = var.attacker_subnet_id
+  source_dest_check = false
 
-  # This is a dirty hack to allow us to serve traffic for 169.254.169.254 as well as use our own IMDS server at
-  # 169.254.169.254 over eth0. Note that we use 169.254.168.254 to for the nginx listen IP not 169.254.169.254 despite
-  # it appearing as the second from other hosts. i.e everyone but us get's the fake IMDS server.
-  #
-  # Reaching our own IMDS is important for normal functioning of the instance, but we also use it as a fallback in nginx
-  # to mock out values. This means that other nodes are accessing our real IMDS server in cases we didn't care to mock
-  # out in nginx.
-  user_data = <<EOH
-#!/usr/bin/env bash
-set -euo pipefail
+  vpc_security_group_ids = [
+    aws_security_group.allow_http.id,
+    aws_security_group.allow_ssh_any.id,
+    aws_security_group.allow_egress_any.id,
+  ]
 
-ip addr add 169.254.168.254/32 dev eth0
-iptables -t nat -A PREROUTING -s 169.254.168.254,172.31.54.46 -d 169.254.169.254/32 -j RETURN
-iptables -t nat -A PREROUTING -d 169.254.169.254/32 -j DNAT --to-destination 169.254.168.254
-iptables -t nat -A POSTROUTING -s 169.254.168.254 -d 169.254.169.254/32 -o eth0 -j MASQUERADE
-service nginx restart
-EOH
+  ami           = "ami-0885b1f6bd170450c" // Ubuntu 20.04
+  instance_type = var.instance_type
 
+  key_name      = module.ssh_key_pair.key_name
+
+  provisioner "remote-exec" {
+    script = "./scripts/setup_imds_proxy.sh"
+
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      host     = self.public_ip
+      private_key = module.ssh_key_pair.private_key
+    }
+  }
+
+  provisioner "file" {
+    source = "./files/imds/"
+    destination = "/var/www/imds"
+
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      host     = self.public_ip
+      private_key = module.ssh_key_pair.private_key
+    }
+  }
 }
--- network.tf
+++ network.tf
@@ -6,6 +6,23 @@ data "aws_subnet" "attacker_subnet" {
   id = var.attacker_subnet_id
 }
 
+resource "aws_security_group" "allow_egress_any" {
+  name        = "allow_egress_any"
+  description = "Allow any outbound traffic"
+  vpc_id      = data.aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_egress_any"
+  }
+}
+
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
   description = "Allow HTTP inbound traffic"
@@ -19,14 +36,25 @@ resource "aws_security_group" "allow_http" {
     cidr_blocks = [data.aws_vpc.main.cidr_block]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_any" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.main.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.main.cidr_block]
   }
 
   tags = {
-    Name = "allow_http"
+    Name = "allow_ssh_any"
   }
 }
